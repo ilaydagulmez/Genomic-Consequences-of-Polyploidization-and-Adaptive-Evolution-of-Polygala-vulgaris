7357cf93e254ece87285f980e637ed20efdf0ab5
ilayda gülmez karaca <163100594+ilaydagulmez@users.noreply.github.com>
2026-08-20 11:26:37 +0300

    Update run_annotation.sh

--- scripts/02_gene_annotation/run_annotation.sh
+++ scripts/02_gene_annotation/run_annotation.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+Helixer.py \
+    --fasta-path genome.fasta \
+    --lineage land_plant \
+    --gff-output-path genome_helixer.gff3
+
+gffread genome_helixer.gff3 \
+    -g genome.fasta \
+    -x genome_CDS.fa \
+    -y genome_protein.fa \
+    -w genome_transcripts.fa
