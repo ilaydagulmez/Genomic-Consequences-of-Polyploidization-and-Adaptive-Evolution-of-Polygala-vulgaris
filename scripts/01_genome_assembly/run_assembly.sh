683b0d9957668018fc05a2b34891f535bc159c55
ilayda gülmez karaca <163100594+ilaydagulmez@users.noreply.github.com>
2026-08-20 11:19:55 +0300

    Update run_assembly.sh

--- scripts/01_genome_assembly/run_assembly.sh
+++ scripts/01_genome_assembly/run_assembly.sh
@@ -22,3 +22,77 @@ SOAPdenovo-127mer scaff \
     -p 64 \
     -g k21_1 \
     -F
+
+redundans.py \
+    -v \
+    -i karect_FASTP_1.fastq karect_FASTP_2.fastq \
+    -f k21_1.scafSeq \
+    --nogapclosing \
+    -o redundans
+
+# Output: redundans/scaffolds.reduced.fa
+
+# Three successive polishing 
+
+cat karect_FASTP_1.fastq karect_FASTP_2.fastq > karect_FASTP_1_2.fastq
+
+python racon_preprocess.py \
+    karect_FASTP_1_2.fastq \
+    > final.racon.fastq
+
+minimap2 \
+    -ax sr \
+    redundans/scaffolds.reduced.fa \
+    final.racon.fastq \
+    > racon_aln.sam
+
+racon \
+    -m 8 \
+    -x -6 \
+    -g -8 \
+    -w 500 \
+    -t 64 \
+    final.racon.fastq \
+    racon_aln.sam \
+    redundans/scaffolds.reduced.fa \
+    > polished1.fasta
+
+minimap2 \
+    -ax sr \
+    polished1.fasta \
+    final.racon.fastq \
+    > 2_racon_aln.sam
+    
+racon \
+    -m 8 \
+    -x -6 \
+    -g -8 \
+    -w 500 \
+    -t 64 \
+    final.racon.fastq \
+    2_racon_aln.sam \
+    polished1.fasta \
+    > polished2.fasta
+
+minimap2 \
+    -ax sr \
+    polished2.fasta \
+    final.racon.fastq \
+    > 3_racon_aln.sam
+
+racon \
+    -m 8 \
+    -x -6 \
+    -g -8 \
+    -w 500 \
+    -t 64 \
+    final.racon.fastq \
+    3_racon_aln.sam \
+    polished2.fasta \
+    > polished3.fasta
+
+ragtag.py scaffold \
+    reference.fasta \
+    polished3.fasta
+
+# Final assembly: ragtag_output/ragtag.scaffold.fasta
